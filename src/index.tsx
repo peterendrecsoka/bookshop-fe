d23301dacda095c41ec52143184a227898b1ecc8
Peter Csoka <petercsoka@gmail.com>
2017-09-23 09:50:24 +0100

    Init main index file

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from "./app"
+
+render((
+    <div>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    </div>
+), document.getElementById('root'))
