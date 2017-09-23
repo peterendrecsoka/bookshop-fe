bc42e76c6394b603eda62812020b755583b1491b
Peter Csoka <petercsoka@gmail.com>
2017-09-23 10:05:46 +0100

    Add header to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Header from "./components/header"
 
 const App = () => (
   <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/book/:id' component={Book}/>
-        <Route path='/cart' component={Cart}/>
-        <Route path='/search/:text' component={Search}/>
+        {/* <Route exact path='/' component={Home}/> */}
+        {/* <Route path='/book/:id' component={Book}/> */}
+        {/* <Route path='/cart' component={Cart}/> */}
+        {/* <Route path='/search/:text' component={Search}/> */}
       </Switch>
   </div>
 )
