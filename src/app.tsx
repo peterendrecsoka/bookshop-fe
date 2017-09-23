0a2088f9e7c5adc3aa881329515211fd716bf552
Peter Csoka <petercsoka@gmail.com>
2017-09-23 10:08:58 +0100

    Add home page to app

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,14 @@ import * as React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from "./components/header"
 
+// Pages
+import Home from "./pages/home"
+
 const App = () => (
   <div>
       <Header />
       <Switch>
-        {/* <Route exact path='/' component={Home}/> */}
+        <Route exact path='/' component={Home}/>
         {/* <Route path='/book/:id' component={Book}/> */}
         {/* <Route path='/cart' component={Cart}/> */}
         {/* <Route path='/search/:text' component={Search}/> */}
